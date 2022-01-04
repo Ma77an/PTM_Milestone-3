241163bf791128a6b07d91e94c51eb9588d0f557
Matan Gabso <matang99@gmail.com>
2022-01-04 23:29:40 +0200

    Milestone #3- Part A: COMPLETE! GRADE: *100*

--- src/test/CLI.java
+++ src/test/CLI.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 import test.Commands.Command;
 import test.Commands.DefaultIO;
 
+import static java.lang.System.exit;
+
 public class CLI {
 
 	ArrayList<Command> commands;
@@ -26,11 +28,25 @@ public class CLI {
 	}
 	
 	public void start() {
-		commands.get(0).execute();
-		commands.get(1).execute();
-		commands.get(2).execute();
-		commands.get(3).execute();
-		commands.get(4).execute();
-		commands.get(5).execute();
+		int option =0;
+//		commands.get(0).execute();
+//		commands.get(1).execute();
+//		commands.get(2).execute();
+//		commands.get(3).execute();
+//		commands.get(4).execute();
+//		commands.get(5).execute();
+		while(true) {
+			dio.write("Welcome to the Anomaly Detection Server.\n" +
+					"Please choose an option:\n");
+			for (int i = 1; i <= 5; i++) {
+				dio.write(commands.get(i).description);
+			}
+			dio.write("6. exit\n");
+			option = (int) dio.readVal();
+			if (option==6){
+				break;
+			}
+			commands.get(option).execute();
+		}
 	}
 }
